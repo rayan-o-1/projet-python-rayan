40db5ab9e7bccad437607bc6e6c6a242b2107c53
rayan o - rtj34 <109901963+rayan-o-1@users.noreply.github.com>
2025-10-18 14:25:16 +0200

    Update affichage_graphique.py

--- Environement 3D/affichage_graphique.py
+++ Environement 3D/affichage_graphique.py
@@ -1,9 +1,26 @@
 import os
-from lib_maths import *
+from lib_math import *
 
 width,height = os.get_terminal_size()
 height -= 1
 pixelBuffer = [' ']*(width*height)
+
+class Camera:
+    def __init__(self,position,pitch,yaw,focalLenth=1) -> None:
+        self.position = position
+        self.pitch = pitch
+        self.yaw = yaw
+        self.focalLenth = focalLenth
+
+    def getLookAtDirection():
+        pass
+
+    def getForwardDirection(self):
+        return vec3(-sin(self.yaw),0,cos(self.yaw))
+    
+    def getRightDirection(self):
+        return vec3( cos(self.yaw),0,sin(self.yaw))
+
 
 def draw():
     print(''.join(pixelBuffer),end='')
@@ -22,36 +39,21 @@ def putTriangle(tri,char):
     def eq(p, a, b):
         return (a.x-p.x)*(b.y-p.y)-(a.y-p.y)*(b.x-p.x)
 
-    xmin = round(min(tri.p1.x, tri.p2.x, tri.p3.x))
-    xmax = round(max(tri.p1.x, tri.p2.x, tri.p3.x))+1
-    ymin = round(min(tri.p1.y, tri.p2.y, tri.p3.y))
-    ymax = round(max(tri.p1.y, tri.p2.y, tri.p3.y))+1
+    xmin = round(min(tri.v1.x, tri.v2.x, tri.v3.x))
+    xmax = round(max(tri.v1.x, tri.v2.x, tri.v3.x))+1
+    ymin = round(min(tri.v1.y, tri.v2.y, tri.v3.y))
+    ymax = round(max(tri.v1.y, tri.v2.y, tri.v3.y))+1
     for y in range(ymin,ymax):
         if 0<=y<height:
             for x in range(xmin,xmax):
                 if 0<=x<width:
                     pos = vec2(x,y)
-                    w1 = eq(pos, tri.p3, tri.p1)
-                    w2 = eq(pos, tri.p1, tri.p2)
-                    w3 = eq(pos, tri.p2, tri.p3)
+                    w1 = eq(pos, tri.v3, tri.v1)
+                    w2 = eq(pos, tri.v1, tri.v2)
+                    w3 = eq(pos, tri.v2, tri.v3)
                     if (w1 >= 0 and w2 >= 0 and w3 >= 0) or (-w1 >= 0 and -w2 >= 0 and -w3 >= 0):
                         putPixel(pos,char)
 
-
-tri = triangle(vec2(10,10),
-               vec2(80,15),
-               vec2(40,30))
-
-tri2 = triangle(vec2(80,30),
-               vec2(100,35),
-               vec2(90,50))
-
-os.system('cls' if os.name == 'nt' else 'clear')
-
-while True:
-    clear(' ')
-    putTriangle(tri, '@')
-    putTriangle(tri2, '@')
-    draw()
-    time.sleep(0.05)
-
+def putMesh(mesh:list[Triangle3D],cam:Camera):
+    for triangle in mesh:
+        putTriangle(triangle.translate(-1*cam.position).rotationY(cam.yaw).rotationX(cam.pitch).projection(cam.focalLenth).toScreen(),'@')
