d0e95ac443994a8bae7bab36df28cfd65c37ee9c
rayan o - rtj34 <109901963+rayan-o-1@users.noreply.github.com>
2026-01-04 23:08:19 +0100

    Update main.py

--- AEGIS/main.py
+++ AEGIS/main.py
@@ -5,30 +5,28 @@ from core.agent import AegisAgent
 def auto_repair_system():
     target_file = "buggy_app.py"
     
-    print(f"🔍 SCANNER: Analyse de '{target_file}' en cours...")
+    print(f"SCANNER: Analyse de '{target_file}' en cours...")
 
     # 1. On tente d'exécuter le fichier pour voir s'il plante
     try:
-        # On lance le script comme un processus externe
         process = subprocess.run(
             [sys.executable, target_file], 
             capture_output=True, 
             text=True
         )
     except FileNotFoundError:
-        print(f"❌ Erreur : Le fichier '{target_file}' n'existe pas.")
+        print(f"Erreur : Le fichier '{target_file}' n'existe pas, ou est intoruvable.")
         return
 
     # 2. Analyse du résultat
     if process.returncode == 0:
-        print("✅ Le fichier s'est exécuté sans erreur. Aucune réparation nécessaire.")
+        print("Le fichier s'est exécuté sans erreur. Aucune réparation nécessaire.")
         print(f"Sortie : {process.stdout}")
         return
     else:
-        print("💥 CRASH DÉTECTÉ !")
+        print("CRASH DÉTECTÉ ")
         
-        # 3. Extraction automatique de l'erreur
-        # On récupère les logs d'erreur
+        # 3. Extraction automatique de l'erreur + récupère les logs d'erreur
         full_error_log = process.stderr.strip()
         lines = full_error_log.split('\n')
         
@@ -41,12 +39,10 @@ def auto_repair_system():
             error_type = "Erreur Inconnue"
             error_msg = last_line
 
-        print(f"🔴 Type: {error_type.strip()}")
-        print(f"🔴 Message: {error_msg.strip()}")
-
-        # 4. Configuration dynamique de l'Agent
-        # On dit à l'agent : "Le script de reproduction, c'est le fichier lui-même"
-        # On laisse le champ vide car le code contient déjà le 'if __main__' qui plante
+        print('\033[31m' + "Type: {error_type.strip()}" + '\33[0m')
+        print('\033[31m' + "Message: {error_msg.strip()}"+ '\33[0m')
+        
+        # On dit à l'agent : "Le script de reproduction, c'est le fichier lui-même" et on laisse le champ vide car le code contient déjà le 'if __main__' qui plante
         reproduction_script = "" 
 
         error_info = {
@@ -58,14 +54,14 @@ def auto_repair_system():
         }
 
         # 5. Lancement d'Aegis
-        print("\n🛡️ ACTIVATION DU PROTOCOLE AEGIS...")
+        print("\n ACTIVATION DU PROTOCOLE AEGIS...")
         agent = AegisAgent(project_path=".")
         success = agent.run_repair_cycle(error_info)
         
         if success:
-            print("✨ BUG CORRIGÉ AVEC SUCCÈS !")
+            print("BUG CORRIGÉ AVEC SUCCÈS !")
         else:
-            print("❌ Échec de la réparation automatique.")
+            print('\033[31m' + "Échec de la réparation automatique."+'\33[0m')
 
 if __name__ == "__main__":
     auto_repair_system()
